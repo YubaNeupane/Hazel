b4a91e47a88db6ec36981f12a50a28dc870cbe33
yuba.neupane <yuba.neupane@pncilab.com>
2025-01-04 00:13:39 -0500

    added a maker

---
+++ premake5.lua
@@ -0,0 +1,102 @@
+workspace "Hazel"
+	architecture "x64"
+	startproject "Sandbox"
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Hazel"
+	location "Hazel"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs{
+			"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+
+		defines{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL",
+		}
+
+		postbuildcommands{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" ..outputdir.. "/Sandbox")
+		}
+		buildoptions "/utf-8"
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+		buildoptions "/utf-8"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		optimize "On"
+		buildoptions "/utf-8"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		optimize "On"
+		buildoptions "/utf-8"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+	objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+	files{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+	includedirs{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
+
+	links{
+		"Hazel"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0"
+		defines{
+			"HZ_PLATFORM_WINDOWS",
+		}
+		buildoptions "/utf-8"
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		buildoptions "/utf-8"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		buildoptions "/utf-8"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		buildoptions "/utf-8"
+		optimize "On"
